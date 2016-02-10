c9d63199474a91fddd0cbbea6eaa42030c5d4327
ymno1129 <ymno1129@gmail.com>
2016-02-10 11:51:08 -0800

    swipe
    added:
    swiping functionality on question page.
    next:
    has to be associated with the question tree.

--- EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/QuestionActivity.java
+++ EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/QuestionActivity.java
@@ -5,14 +5,62 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 public class QuestionActivity extends AppCompatActivity {
+
+
+    final int RIGHT = 0;//Indicator of swiping right.
+    final int LEFT = 1;//swiping left.
+
+    GestureDetector gestureDetector;
+    TextView questionBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questionsfragment);
+        questionBox = (TextView)this.findViewById(R.id.questions);
+        gestureDetector = new GestureDetector(this,onGestureListener);
     }
 
+
+    private GestureDetector.OnGestureListener onGestureListener =
+            new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                       float velocityY) {
+                    float x = e2.getX() - e1.getX();
+                    if (x > 100) {
+                        someAction(RIGHT);
+                    } else if (x < -100) {
+                        someAction(LEFT);
+                    }
+                    return true;
+                }
+            };
+
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    /**
+     * @param action indicating swipe movement
+     * has to be associated with the tree in the future.
+     */
+    public void someAction(int action) {
+        switch (action) {
+            case RIGHT:
+                questionBox.setText("swiped right!");
+                //TODO
+                break;
+            case LEFT:
+                questionBox.setText("swiped left!");
+                //TODO
+                break;
+        }
+    }
 }
