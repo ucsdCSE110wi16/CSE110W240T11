45c2eac2e46d2d4d90307591c84f696e431f5a46
yehzhang <simonzhangintp@gmail.com>
2016-02-25 22:19:45 -0800

    Add import

--- EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/QuestionActivity.java
+++ EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/QuestionActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
