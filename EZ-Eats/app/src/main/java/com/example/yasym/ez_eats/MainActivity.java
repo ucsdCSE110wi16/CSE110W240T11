290a69ec523f05ec7070e71378cf3ba34bb8a8c0
ymno1129 <ymno1129@gmail.com>
2016-02-28 18:32:49 -0800

    change main page
    changed:
    main page background image change.
    main page now swipe to question page

--- EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/MainActivity.java
+++ EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/MainActivity.java
@@ -3,53 +3,76 @@ package com.example.yasym.ez_eats;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.yasym.ez_eats.Yelp.Task.CurrentLocation;
 import com.example.yasym.ez_eats.Yelp.Task.exampleLoadBusinessEntryTask;
 import com.example.yasym.ez_eats.Yelp.Yelp;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity{
 
-    ImageButton btn_start;
+    private final int RIGHT = 0;//Indicator of swiping right.
+    private final int LEFT = 1;//swiping left.
+
     TextView status;
     public static CurrentLocation lastKnownPlace;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-        btn_start = (ImageButton) this.findViewById(R.id.startButton);
-        btn_start.setOnClickListener(this);
-        status = (TextView) this.findViewById(R.id.status);
+        gestureDetector = new GestureDetector(this.onGestureListener);
         lastKnownPlace = new CurrentLocation(this);
 
-        // Tests
-        Button changeText = (Button) findViewById(R.id.testButton);
-        changeText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new exampleLoadBusinessEntryTask(MainActivity.this).execute();
-            }
-        });
-        // Tests end
     }
 
-    public void startButtonClicked() {
+    /**
+     * set the gesture listener to take left and right swipe.
+     */
+    private GestureDetector.OnGestureListener onGestureListener =
+            new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                       float velocityY) {
+                    float x = e2.getX() - e1.getX();
+                    if (x > 100) {
+                        takeAction(RIGHT);
+                    } else if (x < -100) {
+                        takeAction(LEFT);
+                    }
+                    return true;
+                }
+            };
+
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    public void toQuestionActivity() {
         startActivity(new Intent("android.intent.action.QUESTION"));
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.startButton:
-                startButtonClicked();
+    /**
+     * @param action indicating swipe movement
+     * has to be associated with the tree in the future.
+     */
+    public void takeAction(int action) {
+        switch (action) {
+            case RIGHT:
+                //TODO
+                Toast.makeText(this, "Swipe left to begin!", Toast.LENGTH_LONG).show();
                 break;
-            default:
+            case LEFT:
+                //TODO
+                toQuestionActivity();
                 break;
         }
     }
--- EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/Yelp/Task/exampleLoadBusinessEntryTask.java
+++ EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/Yelp/Task/exampleLoadBusinessEntryTask.java
@@ -34,10 +34,10 @@ public class exampleLoadBusinessEntryTask extends LoadBusinessesTask {
         Business b0 = result.get(0);
         Log.i(LOG_TAG, "Got " + result.size() + " businesses.");
         Log.i(LOG_TAG, "The first one is \"" + b0.name + "\"");
-        ((TextView) a.findViewById(R.id.testTextView)).setText(b0.name);
-        ((ImageView) a.findViewById(R.id.testImageView)).setImageDrawable(b0.image);
-        ((ImageView) a.findViewById(R.id.testImageStars)).setImageDrawable(b0.ratingImage);
-        ((ImageView) a.findViewById(R.id.testImageStarsSmall)).setImageDrawable(b0.ratingImageSmall);
-        ((ImageView) a.findViewById(R.id.testImageStarsLarge)).setImageDrawable(b0.ratingImageLarge);
+//        ((TextView) a.findViewById(R.id.testTextView)).setText(b0.name);
+//        ((ImageView) a.findViewById(R.id.testImageView)).setImageDrawable(b0.image);
+//        ((ImageView) a.findViewById(R.id.testImageStars)).setImageDrawable(b0.ratingImage);
+//        ((ImageView) a.findViewById(R.id.testImageStarsSmall)).setImageDrawable(b0.ratingImageSmall);
+//        ((ImageView) a.findViewById(R.id.testImageStarsLarge)).setImageDrawable(b0.ratingImageLarge);
     }
 }
