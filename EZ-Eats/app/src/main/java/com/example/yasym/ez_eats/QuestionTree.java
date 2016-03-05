1e7d9171b82142c78b65ba024e1f7863830274fa
yasymandi <yasymandi@gmail.com>
2016-03-05 13:10:32 -0800

    Commit for Sync

--- EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/QuestionTree.java
+++ EZ-Eats/app/src/main/java/com/example/yasym/ez_eats/QuestionTree.java
@@ -14,35 +14,81 @@ public class QuestionTree {
     }
 
     void ConstructTree(){
-        root = new QuestionNode("Do you want a full meal?", false);
-        QuestionNode DorE = new QuestionNode("Do you want solid foods?", false);
-        QuestionNode Ame_N = new QuestionNode("Do you want a hearty, protein packed meal?", false); //do you want American food
-        QuestionNode Asi_N = new QuestionNode("Do you want food with lots of different spices? ", false); //do you want Asian food
-        QuestionNode Eur_N = new QuestionNode("Do you want something new and exciting?", false); //do you want European food
-        QuestionNode Ame = new QuestionNode("You might like American food!",true);
-        QuestionNode Asi = new QuestionNode("You might like Asian food!!",true);
-        QuestionNode Eur = new QuestionNode("You might like European food!!", true);
-        QuestionNode drink = new QuestionNode("You might be thirsty!", true);
-        QuestionNode eat = new QuestionNode("You're hungry!", true);
-        QuestionNode end = new QuestionNode("I don't think you're hungry!", true);
-        root.setLeft(DorE);
-        DorE.setLeft(drink);
-        DorE.setRight(eat);
-        root.setRight(Ame_N);
-        Ame_N.setLeft(Ame);
-        Ame_N.setRight(Asi_N);
-        Asi_N.setLeft(Asi);
-        Asi_N.setRight(Eur_N);
-        Eur_N.setLeft(Eur);
-        Eur_N.setRight(end);
+        root = new QuestionNode("Are you super hungry?", false);
 
-        drink.setResults(ResultingCategories.getdrinks());
-        eat.setResults(ResultingCategories.getFoods());
-        Ame.setResults(ResultingCategories.getAmericans());
-        Eur.setResults(ResultingCategories.getEuropean());
-        Asi.setResults(ResultingCategories.getAsian());
-        end.setResults(ResultingCategories.getAll());
-        root.setResults(ResultingCategories.getAll());
+        QuestionNode thirsty = new QuestionNode("Are you thirsty?", false); //root no
+        QuestionNode hurry = new QuestionNode("Are you in a hurry?", false); //root yes
+
+        QuestionNode lightMeal = new QuestionNode("Would you like a light meal?", false);//thirsty no
+        QuestionNode healthDrink = new QuestionNode("Are you trying to be healthy?", false); //thirsty yes
+
+        QuestionNode snack = new QuestionNode("I think you want a snack!", true); //light meal no
+        QuestionNode lightFoods = new QuestionNode("You want something light!", true); //light meal yes
+
+        QuestionNode fastFood = new QuestionNode("Do you want fast food?", false); //hurry yes
+        QuestionNode sitDownRest = new QuestionNode("Do you want to go to a sit down restaurant?", false); //hurry no
+
+        QuestionNode normalFoods = new QuestionNode("Here are quick things you can grab!", true);//fastFood no
+        QuestionNode junkFood = new QuestionNode("Here's are local fast foods!", true);//fastFood yes
+
+        QuestionNode healthDrinkRes = new QuestionNode("Here are some healthy drink options for you!", true);//healthDrink yes
+        QuestionNode drinks = new QuestionNode("Here are your drink options!", true); //healthDrink no
+
+
+        QuestionNode vegetarian = new QuestionNode("Are you a vegetarian?", false); //sitDownRest yes
+        //sitDownRest no goes to fastFood node
+
+        QuestionNode vegFoods = new QuestionNode("Here are some vegtastic options!", true);//vegetarian yes
+        QuestionNode american = new QuestionNode("Are you in the mood for American food?", false);//vegetarian no
+
+        QuestionNode americanFoods = new QuestionNode("Here are good ol' American restaurats!", true);//american yes
+        QuestionNode asian = new QuestionNode("Are you in the mood for Asian food?", false);//american no
+
+        QuestionNode asianFoods = new QuestionNode("Here are Asian foods near you!", true);//asian yes
+        QuestionNode allElse = new QuestionNode("You may like one of these!", true);//asian no
+
+        root.setLeft(thirsty);
+        root.setRight(hurry);
+
+        thirsty.setLeft(lightMeal);
+        thirsty.setRight(healthDrink);
+
+        healthDrink.setRight(healthDrinkRes);
+        healthDrink.setLeft(drinks);
+
+        lightMeal.setLeft(snack);
+        lightMeal.setRight(lightFoods);
+
+        hurry.setRight(fastFood);
+        hurry.setLeft(sitDownRest);
+
+        fastFood.setLeft(normalFoods);
+        fastFood.setRight(junkFood);
+
+        sitDownRest.setLeft(fastFood);
+        sitDownRest.setRight(vegetarian);
+
+
+        vegetarian.setRight(vegFoods);
+        vegetarian.setLeft(american);
+
+        american.setRight(americanFoods);
+        american.setLeft(asian);
+
+        asian.setRight(asianFoods);
+        asian.setLeft(allElse);
+
+        //TO DO: set snack results to: grocery store, gas station, frozen yogurt
+        //TO DO: set lightFoods result to: salads, soups, frozen yogurt, sandwiches
+        //TO DO: set healthDrinkRes result to: juices, organic drinks
+        //TO DO: set drinks result to: coffee shops, juices, any drinks
+        //TO DO: set normalFoods: pizza, subway, chipotle, etc.
+        //TO DO: set junkFood to: burgers, taco bell, del taco, any other fast food places
+        //TO DO: set vegOptions: vegetarian stuff
+        //TO DO: set americanFoods : american foods
+        //TO DO: set asianFoods : asian foods
+        //TO DO: set allElse : European, African, and all other foods not American or Asian
+        
     }
 
     QuestionNode getRoot(){
